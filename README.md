de8ab778aa0bf34a79c008e43b5c122284b3003c
Renan Martins <martins@fujisan.co.jp>
2017-11-07 14:39:26 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Auth with GraphQL
+
+Install all dependencies
+
+```
+npm install
+```
+
+For development, use
+
+```
+npm run dev
+```
+
+To run the app once, without nodemon:
+
+```
+npm start
+```
---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => {
+  return (
+    <div>
+      Auth with GraphQL
+    </div>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
