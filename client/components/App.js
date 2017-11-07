f93d25c618efeb71a0d53a7c086193511d837b42
Renan Martins <martins@fujisan.co.jp>
2017-11-07 15:40:39 -0800

    Add App component with a Navbar

---
+++ client/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Navbar from './/Navbar';
+
+export default () => (
+  <div className="ui container">
+    <Navbar />
+    Auth with GraphQL
+  </div>
+);
---
+++ client/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+  <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Auth with GraphQL</a>
+      <ul id="nav-mobile" className="right hide-on-small-only">
+        <li><a href="#">Sign Up</a></li>
+        <li><a href="#">Sign In</a></li>
+        <li><a href="#">Sign Out</a></li>
+      </ul>
+    </div>
+  </nav>
+);
