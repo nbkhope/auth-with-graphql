3d689230cd24b425a7d0bb593d4dd7f39fbecede
Renan Martins <martins@fujisan.co.jp>
2017-11-15 10:22:10 -0800

    refs #master [+] Show auth actions in navbar depending on whether user is logged in

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
-export default () => (
-  <nav>
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo">Auth with GraphQL</a>
-      <ul id="nav-mobile" className="right hide-on-small-only">
-        <li><a href="#">Sign Up</a></li>
-        <li><a href="#">Sign In</a></li>
-        <li><a href="#">Sign Out</a></li>
-      </ul>
-    </div>
-  </nav>
-);
+const renderAuthActions = ({ data }) => {
+  if (data.loading) {
+    return <div />;
+  }
+  else if (data.user) {
+    return <li><a href="#">Sign Out</a></li>;
+  }
+
+  // else, if not logged in
+  return [
+    <li key="signup"><a href="#">Sign Up</a></li>,
+    <li key="signin"><a href="#">Sign In</a></li>
+  ];
+};
+
+const Navbar = (props) => {
+  console.log(props);
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">Auth with GraphQL</a>
+        <ul id="nav-mobile" className="right hide-on-small-only">
+          {renderAuthActions(props)}
+        </ul>
+      </div>
+    </nav>
+  )
+};
+
+export default graphql(query)(Navbar);
